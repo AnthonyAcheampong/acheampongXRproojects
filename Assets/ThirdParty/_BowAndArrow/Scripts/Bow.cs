9a4de2c100e0e46fc4bab80c289f14b7d41398b4
AnthonyAcheampong <117184182+AnthonyAcheampong@users.noreply.github.com>
2025-09-23 10:48:42 -0400

    Added Haptics and Sound Effects to Bow

--- Assets/ThirdParty/_BowAndArrow/Scripts/Bow.cs
+++ Assets/ThirdParty/_BowAndArrow/Scripts/Bow.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using UnityEngine;
+using Oculus.Haptics; //Library for Haptics
+using UnityEngine.XR.OpenXR.Input;
 
 public class Bow : MonoBehaviour
 {
@@ -20,9 +22,18 @@ public class Bow : MonoBehaviour
 
     public float pullMultiplier = 1;
 
+    [Header("Haptics")]
+    public HapticClipPlayer hapticClipPlayer; //Reference for playing Haptic Studio Files
+
+    [Header("Audio")]
+    public AudioClip bowReleaseSound;
+    private AudioSource bowAudioSource;
+
+
     private void Awake()
     {
         m_Animator = GetComponent<Animator>();
+        bowAudioSource = GetComponent<AudioSource>();
     }
 
     private void Start()
@@ -91,6 +102,7 @@ public class Bow : MonoBehaviour
         // If we've pulled far enough, fire
         if (m_PullValue > 0.25f)
             FireArrow();
+            PlayHapticFeedback();
 
         // Clear
         m_PullingHand = null;
@@ -109,4 +121,27 @@ public class Bow : MonoBehaviour
         m_CurrentArrow.Fire(m_PullValue * pullMultiplier);
         m_CurrentArrow = null;
     }
+
+    private void PlayHapticFeedback()
+    {
+        if (m_PullingHand != null) 
+        {
+            HapticSource hapticSource = m_PullingHand.GetComponent<HapticSource>();
+            if (hapticSource != null)
+            {
+                hapticSource.Play();
+        
+            }
+
+            if (bowAudioSource != null && bowReleaseSound != null)
+            { 
+                bowAudioSource.PlayOneShot(bowReleaseSound);
+            
+            }
+
+            
+        }
+    }
+
 }
+
