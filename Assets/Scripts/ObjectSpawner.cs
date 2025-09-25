35aabfff96d6e6141cb4df1748490227f1d69cbd
AnthonyAcheampong <117184182+AnthonyAcheampong@users.noreply.github.com>
2025-09-25 10:50:43 -0400

    Added 3D objects to spawn

---
+++ Assets/Scripts/ObjectSpawner.cs
@@ -0,0 +1,36 @@
+using System.Security.Cryptography;
+using UnityEngine;
+
+/*
+ 
+Select to spawn
+where object spawns
+Cooldown period
+Input button
+hand
+ 
+
+*/
+
+public class ObjectSpawner : MonoBehaviour
+{
+    public GameObject objectPrefab: //object to spawn
+    public Transform spawnPoint; //where it spanws
+    public XRNode controllerNode = XRNode.RightHand;
+    public float spawnCooldown = 1.0f; // Need a coroutine
+    private bool canSpawn = true;
+    
+    
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
